1a86646e17b28a166dcf9a4f7e32daf5c00d4643
Giannis Dimoulios <giannis.dimoulios@gmail.com>
2024-05-17 18:14:05 +0300

    problem 3 text

--- src/prob3.py
+++ src/prob3.py
@@ -1,13 +1,13 @@
 import numpy as np
 
-def breakString(n, L):
-    m = len(L)
-    L = [-1] + L + [n-1]
+def breakString(n, B):
+    m = len(B)
+    B = [-1] + B + [n-1]
 
-    cost = np.full(shape=(m+2, m+2), fill_value=np.inf)
+    time_units = np.full(shape=(m+2, m+2), fill_value=np.inf)
 
     for i in range(m+1):
-        cost[i, i+1] = 0
+        time_units[i, i+1] = 0
 
     for delta in range(2, m + 2):
         for i in range(0, m + 2 - delta):
@@ -15,10 +15,10 @@ def breakString(n, L):
 
             minimum = np.inf
             for k in range(i+1, j):
-                costValue = cost[i, k] + cost[k, j] + L[j] - L[i]
-                if costValue < minimum:
-                    minimum = costValue
+                time_curr = time_units[i, k] + time_units[k, j] + B[j] - B[i]
+                if time_curr < minimum:
+                    minimum = time_curr
 
-            cost[i, j] = minimum
+            time_units[i, j] = minimum
 
-    return cost[0, m+1]
+    return time_units[0, m+1]
